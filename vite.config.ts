9bc824a4ed065625dab0c96f58b5b318e5f56c10
James Dow <james.dow@klaviyo.com>
2026-02-11 11:16:56 -0500

    chore: auto sync

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,15 @@
 /// <reference types="node" />
-import { defineConfig } from 'vite'
+import path from 'path'
+import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
 
 export default defineConfig({
   base: process.env.BASE_PATH || '/',
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: { '@': path.resolve(__dirname, './src') },
+  },
   server: {
     host: '0.0.0.0',
     port: process.env.PORT ? parseInt(process.env.PORT, 10) : 5173,
